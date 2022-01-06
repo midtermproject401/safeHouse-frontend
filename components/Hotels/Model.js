205d90d34f02d7a7a53de490d6d488a94d3e99df
HaninHaidrah <haneenhadey@gmail.com>
2022-01-06 03:14:31 +0200

    de

--- components/Hotels/Model.js
+++ components/Hotels/Model.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import { Button, Form } from "react-bootstrap";
-import swal from 'sweetalert';
+import swal from "sweetalert";
+import Calendar from "react-calendar";
+import { RiVisaLine } from "react-icons/ri";
+import { FaCcMastercard } from "react-icons/fa";
+import { GiCash } from "react-icons/gi";
+
+
+
 
 import styles from "../../styles/Model.module.css";
 
@@ -9,14 +16,24 @@ export default function ModelBooking() {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [showStatus, setShowStatus] = useState(false);
+  const [value, changeValue] = useState(new Date());
 
   const handleClick = (e) => {
     e.preventDefault();
     swal({
-        title: "Thanks for booking",
-        text: "your booking went succesfully we will check your validation of payment!",
-        icon: "success",
-      });  };
+      title: "Thanks for booking",
+      text: "your booking went succesfully we will check your validation of payment!",
+      icon: "success",
+    });
+  };
+
+  const handleCheckValue = (e) => {
+    e.preventDefault();
+    e.target.value = value;
+    console.log(e.target.value);
+    setShowStatus(!showStatus);
+  };
 
   return (
     <div>
@@ -25,43 +42,72 @@ export default function ModelBooking() {
         Book NOW !
       </Button>
       <Modal show={show} onHide={handleClose} className={styles.model}>
-        <Modal.Header closeButton>
+        <Modal.Header closeButton className={styles.formid}>
           <Modal.Title>Booking The Room</Modal.Title>
         </Modal.Header>
 
-        <Modal.Body>
-          <Form>
+        <Modal.Body className={styles.formid}>
+          <Form className={styles.formid}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter your first name" />
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
+              {/* <Form.Label>First Name</Form.Label> */}
+              <Form.Control
+                type="text"
+                placeholder="Enter your first name"
+                className={styles.formid}
+              />{" "}
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                className={styles.formid}
+              />
+              <Form.Text className="text-muted" >
                 We will never share your email with anyone else.
               </Form.Text>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Check in Date</Form.Label>
-              <Form.Control type="text" placeholder="Check in Date" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Check out Date</Form.Label>
-              <Form.Control type="text" placeholder="Check out Date" />
+              {/* <Form.Label>Check in Date</Form.Label> */}
+              <Form.Control
+                type="text"
+                placeholder="Check-in-Date -check-out-Date"
+                className={styles.formid}
+                onClick={handleCheckValue}
+              />
+              {showStatus && (
+                <div className={styles.calender}>
+                  {" "}
+                  <Calendar
+                    onChange={(date) => changeValue(date)}
+                    value={value}
+                    showTimeInput
+                    timeInputLabel="CheckinDate"
+                    dateFormat="yyyy/MM/dd"
+                  />
+                </div>
+              )}
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
+              <Form.Select type="select" label="ChOse Your Payment Method" >
+                  <option value="1"> Visa </option>
+                  <option value="2">Master</option>
+                  <option value="3">Cash </option>{" "}
+                </Form.Select>
             </Form.Group>
             <Button variant="primary" type="submit" onClick={handleClick}>
-              Submit
+              Confirm Booking
             </Button>
           </Form>{" "}
         </Modal.Body>
 
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+        <Modal.Footer className={styles.formFooter}>
+          {/* <Button variant="secondary" onClick={handleClose}>
             Close
-          </Button>
+          </Button> */}
+          <h6> Safe-House.com</h6>
+          <img src="/img/logoNew.PNG"></img>
           {/* <Button variant="primary">Save changes</Button> */}
         </Modal.Footer>
       </Modal>
--- pages/home/Advs.js
+++ pages/home/Advs.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useForm } from "react-hook-form";
 import styles from "../../styles/Adv.module.css";
 import axios from "axios";
 import { RiAdvertisementFill } from "react-icons/ri";
+import { FcHome } from "react-icons/fc";
+
+import Modal from "react-bootstrap/Modal";
+import { Button, Form } from "react-bootstrap";
+import { useState } from "react";
+import swal from "sweetalert";
+import {
+  MdDescription,
+  MdPriceCheck,
+  MdOutlineShareLocation,
+  MdEventAvailable,
+  MdHotel,
+} from "react-icons/md";
 
 export default function Advs() {
   const { register, handleSubmit } = useForm();
@@ -10,6 +23,21 @@ export default function Advs() {
   //   console.log(data);
   //   console.log("hell0");
   // }
+
+  // const inputRef = useRef<HTMLInputElement>(null);
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    swal({
+      title: "Your ad post successfully",
+      text: "You can see now your ad adds to Home Page",
+      icon: "success",
+    });
+  };
   async function onSubmit(values) {
     let config = {
       method: "post",
@@ -30,110 +58,241 @@ export default function Advs() {
     } catch (err) {}
   }
 
+  const handleUpload = () => {
+    alert("upload your images");
+  };
+
   return (
     <>
-      <div className={styles.form}>
-        <div className={styles.title}>
-          Post your Advertisment <RiAdvertisementFill />
-        </div>
-        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-          <div className={styles.input_box}>
-            <span>location</span>
-            <input
-              {...register("location", { required: true })}
-              placeholder="location"
-              className={styles.input}
-            />
-          </div>
-          <div className={styles.input_box}>
-            <span>Description</span>
-            <input
-              {...register("Description", { required: true })}
-              placeholder="Description"
-              className={styles.input}
-            />
-          </div>
-          <div className={styles.input_box}>
-            <span>Main imge</span>
-            <input
-              {...register("imgHero", { required: true })}
-              placeholder="Main imge"
-              className={styles.input}
-            />
-          </div>
-          <div className={styles.input_box}>
-            <span>imge 1</span>
-            <input
-              {...register("img1", { required: true })}
-              placeholder="imge 1"
-              className={styles.input}
-            />
-          </div>
-          <div className={styles.input_box}>
-            <span>imge 2</span>
-            <input
-              {...register("img2", { required: true })}
-              placeholder="imge 2"
-              className={styles.input}
-            />
-          </div>
-          <div className={styles.input_box}>
-            <span>Rent Duration</span>
-            <input
-              {...register("rentDuration", { required: true })}
-              placeholder="Rent Duration"
-              className={styles.input}
-            />
-          </div>
-          <div className={styles.input_box}>
-            <span>imge 3</span>
-            <input
-              {...register("img3", { required: true })}
-              placeholder="imge 3"
-              className={styles.input}
-            />
-          </div>
-          <div className={styles.input_box}>
-            <span>price</span>
-            <input
-              {...register("price", { required: true })}
-              placeholder="price"
-              className={styles.input}
-            />
-          </div>
-          <div className={styles.input_box}>
-            <span>state</span>
-            <input
-              {...register("state", { required: true })}
-              placeholder="state"
-              className={styles.input}
-            />
-          </div>
-          <div className={styles.input_box}>
-            <span> Your Name</span>
-            <input
-              {...register("ownerName", { required: true })}
-              placeholder="Your Name"
-              className={styles.input}
-            />
-          </div>
-          <div className={styles.input_box}>
-            <span>Phone Number</span>
-            <input
-              {...register("phoneNumber", { required: true })}
-              placeholder="Phone Number"
-              className={styles.input}
-            />
-          </div>
-          <div>
-            <input
-              type="submit"
-              className={styles.form_button}
-              value={"post an AD"}
-            />
-          </div>
-        </form>
+      {" "}
+      <div className={styles.formDiv}>
+        
+        <img src="https://thumbs.dreamstime.com/b/real-estate-agent-abstract-concept-vector-illustration-market-demonstrating-house-buying-new-appartment-realtor-commercial-198853654.jpg"></img>
+        <Button onClick={handleShow} className={styles.bt}>
+          {" "}
+          Post Your Ad to be published <RiAdvertisementFill />
+        </Button>
+        <p>Start posting your ads </p>
+        <Modal show={show} onHide={handleClose} className={styles.model}>
+          <Modal.Header closeButton className={styles.formid}>
+            <Modal.Title className={styles.formid}>
+              {" "}
+              Post your Advertisment <RiAdvertisementFill />
+            </Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body className={styles.formid}>
+            <Form className={styles.formid}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                {/* <Form.Label>
+                  {" "}
+                  <MdPriceCheck />
+                </Form.Label> */}
+                <Form.Control
+                  type="text"
+                  placeholder="location"
+                  className={styles.formid}
+                />
+                {/* <Form.Label>Description</Form.Label> */}
+              </Form.Group>
+              <Form.Group>
+                <Form.Control
+                  type="text"
+                  placeholder="Description"
+                  className={styles.formid}
+                />
+              </Form.Group>
+
+              <Form.Group
+                className="mb-3"
+                controlId="formBasicPassword"
+                className={styles.formid}
+              >
+                {/* <Form.Label>Add Images for your house</Form.Label> */}
+                <div className="m-3">
+                  <label className="mx-3">
+                    Upload your images for your house{" "}
+                  </label>
+                  <input className="d-none" type="file" />
+                  <button
+                    onClick={handleUpload}
+                    className="btn btn-outline-primary"
+                  >
+                    Upload
+                  </button>
+                </div>
+                {/* <Form.Control
+                  type="text"
+                  placeholder="Add Images for your house"
+                /> */}
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Select className={styles.formid}>
+                  <option> Chooses your Rent Duration</option>
+                  <option value="1">weekly</option>
+                  <option value="2">daily</option>
+                  <option value="3">yearly</option>{" "}
+                </Form.Select>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                {/* <Form.Label>Add The price </Form.Label> */}
+                <Form.Control
+                  type="text"
+                  placeholder="Add The price"
+                  className={styles.formid}
+                />
+              </Form.Group>
+              <Button
+                type="submit"
+                onClick={handleClick}
+                className={styles.butt}
+              >
+                Post The Ads
+              </Button>
+            </Form>{" "}
+          </Modal.Body>
+
+          {/* <div className={styles.title}></div>
+          <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+            <div className={styles.input_box}>
+              <span>location</span>
+              <input
+                {...register("location", { required: true })}
+                placeholder="location"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_box}>
+              <span>Description</span>
+              <input
+                {...register("Description", { required: true })}
+                placeholder="Description"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_box}>
+              <span>Main imge</span>
+              <input
+                {...register("imgHero", { required: true })}
+                placeholder="Main imge"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_box}>
+              <span>imge 1</span>
+              <input
+                {...register("img1", { required: true })}
+                placeholder="imge 1"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_box}>
+              <span>imge 2</span>
+              <input
+                {...register("img2", { required: true })}
+                placeholder="imge 2"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_box}>
+              <span>Rent Duration</span>
+              <input
+                {...register("rentDuration", { required: true })}
+                placeholder="Rent Duration"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_box}>
+              <span>imge 3</span>
+              <input
+                {...register("img3", { required: true })}
+                placeholder="imge 3"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_box}>
+              <span>price</span>
+              <input
+                {...register("price", { required: true })}
+                placeholder="price"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_box}>
+              <span>state</span>
+              <input
+                {...register("state", { required: true })}
+                placeholder="state"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_box}>
+              <span> Your Name</span>
+              <input
+                {...register("ownerName", { required: true })}
+                placeholder="Your Name"
+                className={styles.input}
+              />
+            </div>
+            <div className={styles.input_box}>
+              <span>Phone Number</span>
+              <input
+                {...register("phoneNumber", { required: true })}
+                placeholder="Phone Number"
+                className={styles.input}
+              />
+            </div>
+            <div>
+              <input
+                type="submit"
+                className={styles.form_button}
+                value={"post an AD"}
+              />
+            </div>
+          </form> */}
+        </Modal>
       </div>
     </>
   );
 }
+
+/* <Modal.Header closeButton>
+<Modal.Title>Booking The Room</Modal.Title>
+</Modal.Header>
+
+<Modal.Body>
+<Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>First Name</Form.Label>
+    <Form.Control type="text" placeholder="Enter your first name" />
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We will never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Check in Date</Form.Label>
+    <Form.Control type="text" placeholder="Check in Date" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Check out Date</Form.Label>
+    <Form.Control type="text" placeholder="Check out Date" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+  <Button variant="primary" type="submit" onClick={handleClick}>
+    Submit
+  </Button>
+</Form>{" "}
+</Modal.Body>
+
+<Modal.Footer>
+<Button variant="secondary" onClick={handleClose}>
+  Close
+</Button>
+{/* <Button variant="primary">Save changes</Button> */
+
+// </Modal.Footer> */}
